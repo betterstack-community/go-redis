81d8ab490a3b384e40ce4ad36c30ed5ffe1cba60
Woo Jia Hao <woojiahao1234@gmail.com>
2023-06-28 00:19:54 +0800

    Setup connection example

--- cmd/connect/connect.go
+++ cmd/connect/connect.go
@@ -1,6 +1,22 @@
-// This f
+// Demonstration of how to connect to a Redis server from Go
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+)
 
+func main() {
+	// Ensure that you have Redis running on your system
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	// Ensure that the connection is properly closed gracefully
+	defer rdb.Close()
+
+	// Perform basic diagnostic to check if the connection is working
+	// Expected result > ping: PONG
+	fmt.Println(rdb.Ping())
 }
